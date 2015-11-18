57cff1dfa8b2c7f10b757a1012f1a1855bcfa0e6
6819710 <j.p.galovic@gmail.com>
2015-11-19 02:01:24 +1100

    Add: Vector2D

---
+++ Engine/Vector2D.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dragon.Engine
+{
+    /// <summary>
+    /// Defines Vector in 2D Space.
+    /// </summary>
+    public class Vector2D
+    {
+        private double _i;
+        private double _j;
+
+        /// <summary>
+        /// Constucts Vector in 2D Space.
+        /// </summary>
+        public Vector2D()
+        {
+            _i = 0;
+            _j = 0;
+        }
+
+        /// <summary>
+        /// Constructs Vector in 2D Space.
+        /// </summary>
+        /// <param name="i">Magnitude in X-Axis.</param>
+        /// <param name="j">Magnitude in Y-Axis.</param>
+        public Vector2D(double i, double j)
+        {
+            _i = i;
+            _j = j;
+        }
+
+        /// <summary>
+        /// Property: I Coordinate.
+        /// </summary>
+        public double I
+        {
+            get
+            {
+                return _i;
+            }
+
+            set
+            {
+                _i = value;
+            }
+        }
+
+        /// <summary>
+        /// Property: J Coordinate.
+        /// </summary>
+        public double J
+        {
+            get
+            {
+                return _j;
+            }
+
+            set
+            {
+                _j = value;
+            }
+        }
+
+        /// <summary>
+        /// Converts Vector to Point in 2D Space.
+        /// </summary>
+        public Point2D Point2D
+        {
+            get
+            {
+                return new Point2D(_i, _j);
+            }
+        }
+    }
+}
