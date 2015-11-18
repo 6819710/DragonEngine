78ba8958720fff6b8882d852619619b8d30993fb
6819710 <j.p.galovic@gmail.com>
2015-11-19 01:40:35 +1100

    Add: Feilds and Constructors.

--- Engine/Point3D.cs
+++ Engine/Point3D.cs
@@ -5,7 +5,36 @@ using System.Text;
 
 namespace Dragon.Engine
 {
+    /// <summary>
+    /// Defines Points in 3D Space.
+    /// </summary>
     public class Point3D
     {
+        private double _x;
+        private double _y;
+        private double _z;
+
+        /// <summary>
+        /// Constructs Point in 3D Space.
+        /// </summary>
+        public Point3D()
+        {
+            _x = 0;
+            _y = 0;
+            _z = 0;
+        }
+
+        /// <summary>
+        /// Constucts Point in 3D Space.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        public Point3D(double x, double y, double z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
     }
 }
