0545f780d2c8304115ad0e5df63ebbbb3c8e174c
6819710 <j.p.galovic@gmail.com>
2015-11-19 01:36:28 +1100

    Add: Feilds and Constuctors.

--- Engine/Point2D.cs
+++ Engine/Point2D.cs
@@ -10,5 +10,27 @@ namespace Dragon.Engine
     /// </summary>
     public class Point2D
     {
+        private double _x;
+        private double _y;
+
+        /// <summary>
+        /// Constructs Point in 2D Space.
+        /// </summary>
+        public Point2D()
+        {
+            _x = 0;
+            _y = 0;
+        }
+
+        /// <summary>
+        /// Constructs Point in 2D Space.
+        /// </summary>
+        /// <param name="x">X Coordinate.</param>
+        /// <param name="y">Y Coordinate.</param>
+        public Point2D(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
     }
 }
