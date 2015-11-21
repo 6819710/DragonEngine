b4d8fa7ee52afe774f91cfa12a5b96ac93f52982
6819710 <j.p.galovic@gmail.com>
2015-11-21 22:43:45 +1100

    Add: Constructor Tests.

--- Dragon.EngineTests/Matrix2DTests.cs
+++ Dragon.EngineTests/Matrix2DTests.cs
@@ -12,15 +12,27 @@ namespace Dragon.Engine.Tests
     public class Matrix2DTests
     {
         [Test()]
-        public void Matrix2DTest()
+        public void Matrix2DTestDefault()
         {
-            Assert.Fail();
+            Matrix2D test = new Matrix2D();
+
+            Assert.IsInstanceOf<Matrix2D>(test);
+            Assert.AreEqual(1, test.A1);
+            Assert.AreEqual(0, test.A2);
+            Assert.AreEqual(0, test.B1);
+            Assert.AreEqual(1, test.B2);
         }
 
         [Test()]
-        public void Matrix2DTest1()
+        public void Matrix2DTestValues()
         {
-            Assert.Fail();
+            Matrix2D test = new Matrix2D(1, 2, 3, 4);
+
+            Assert.IsInstanceOf<Matrix2D>(test);
+            Assert.AreEqual(1, test.A1);
+            Assert.AreEqual(2, test.A2);
+            Assert.AreEqual(3, test.B1);
+            Assert.AreEqual(4, test.B2);
         }
     }
 }
