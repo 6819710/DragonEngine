81376aca02113407af3e6941f63c6ffc3a706685
6819710 <j.p.galovic@gmail.com>
2015-11-21 23:15:51 +1100

    Add: Translation and Rotation for Point 2D

---
+++ Engine/Math/Transformation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dragon.Engine.Mathematics
+{
+    /// <summary>
+    /// Provides Transformation Tools.
+    /// </summary>
+    public static class Transformation
+    {
+        /// <summary>
+        /// Translates Given Point by Given Vector
+        /// </summary>
+        /// <param name="point">Point to Offset.</param>
+        /// <param name="vector">Offset Vector.</param>
+        /// <returns>Point2D.</returns>
+        public static Point2D TranslatePoint2D(Point2D point, Vector2D vector)
+        {
+            return new Point2D(point.X + vector.I, point.Y + vector.J);
+        }
+
+        /// <summary>
+        /// Rotates Given Point about Origin by Given Angle in Radians.
+        /// </summary>
+        /// <param name="Angle">Angle in Radians.</param>
+        /// <returns>Point2D.</returns>
+        public static Point2D RotatePoint2D(double Angle)
+        {
+            return new Point2D
+        }
+    }
+}
